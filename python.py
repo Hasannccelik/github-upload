b4d6f4f100b433007aa08b61d24eccd0de87acc8
Hasannccelik <fdaclock159@gmail.com>
2019-11-26 00:40:12 +0300

    gönderme

---
+++ python.py
@@ -0,0 +1,33 @@
+#Olmazsa olmazdır easy-peasy
+print("Hello World")
+x=5
+y="Merhaba Dünya"
+#basit if tanımı
+if 5>2:
+    print("5 büyüktür 2 den") 
+    
+print(x) 
+print(y)
+    #Yorum satırı
+#sıralı bir biçimde değişken tanımlama
+a,b,c="orange","banana","cherry"
+print(a)
+print(b)
+print(c)
+#tek satırda aynı tipte değişken tanımlama
+d=e=f="Falcon"
+print(d)
+print(e)
+print(f)
+#print sonuna değişken gösterme
+aw="Awesome"
+print("Python is "+aw)
+#iki string i bir string gösterme
+pi="Pythonis"
+aw="Awesome"
+tl=pi+aw
+print(tl)
+
+     
+
+
